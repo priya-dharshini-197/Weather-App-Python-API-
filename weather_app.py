de7720508b485de2069b2ef146dc7084769b8b1e
Priyadharshini K <priyadharshinik0719@gmail.com>
2026-03-23 14:54:27 +0530

    Added humidity feature

--- weather_app.py
+++ weather_app.py
@@ -8,5 +8,6 @@ response = requests.get(url)
 data = response.json()
 
 temperature = data["current_condition"][0]["temp_C"]
+humidity = data["current_condition"][0]["humidity"]
 
-print(f"Temperature in {city} is {temperature}°C")
+print(f"Temperature in {city} is {temperature}°C and humidity is {humidity
